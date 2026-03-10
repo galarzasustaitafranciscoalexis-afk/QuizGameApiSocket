583fbb90a003749e62af276be4daadc5e0928189
wilkes00 <oleonaguilar@gmail.com>
2026-03-09 20:21:42 -0600

    Clase ConexionBD creada, funciona la correctamente la conexion de la base de datos

---
+++ QuizGame/Clases/ConexionBD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace QuizGame.Clases
+{
+    internal class ConexionBD
+    {
+        MySqlConnection conn;
+        public ConexionBD()
+        {
+            conn = new MySqlConnection("server=localhost;user=root;database=quiz_game;port=3306;password=root;");
+        }
+
+        public void recuperarCategoria()
+        {
+            try
+            {
+                conn.Open();
+                string sql = "SELECT * FROM categoria";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["id_categoria"] + " - " + reader["nombre_categoria"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuizGame.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace QuizGame
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Menu());
+            ConexionBD conexion = new ConexionBD();
+            conexion.recuperarCategoria();
         }
     }
 }
