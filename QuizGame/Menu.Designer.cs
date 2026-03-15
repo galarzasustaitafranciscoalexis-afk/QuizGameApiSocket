ff726549d6c90482934a0339eba4a51bc1a00e04
Kevin <kevin@gmail.com>
2026-03-15 14:10:08 -0600

    Inserción de preguntas y respuestas en la base de datos

--- QuizGame/Menu.Designer.cs
+++ QuizGame/Menu.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.fondo = new QuizGame.ControlesPersonalizados.FondoRadial();
-            this.btn_cat1 = new QuizGame.BotonesPersonalizados.Boton();
-            this.btn_cat2 = new QuizGame.BotonesPersonalizados.Boton();
-            this.btn_cat3 = new QuizGame.BotonesPersonalizados.Boton();
-            this.btn_cat4 = new QuizGame.BotonesPersonalizados.Boton();
-            this.btn_cat5 = new QuizGame.BotonesPersonalizados.Boton();
             this.label1 = new System.Windows.Forms.Label();
+            this.btn_cat5 = new QuizGame.BotonesPersonalizados.Boton();
+            this.btn_cat4 = new QuizGame.BotonesPersonalizados.Boton();
+            this.btn_cat3 = new QuizGame.BotonesPersonalizados.Boton();
+            this.btn_cat2 = new QuizGame.BotonesPersonalizados.Boton();
+            this.btn_cat1 = new QuizGame.BotonesPersonalizados.Boton();
             this.fondo.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,77 +53,16 @@
             this.fondo.Size = new System.Drawing.Size(715, 367);
             this.fondo.TabIndex = 2;
             // 
-            // btn_cat1
+            // label1
             // 
-            this.btn_cat1.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat1.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btn_cat1.BorderRadius = 40;
-            this.btn_cat1.BorderSize = 0;
-            this.btn_cat1.FlatAppearance.BorderSize = 0;
-            this.btn_cat1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cat1.ForeColor = System.Drawing.Color.White;
-            this.btn_cat1.Location = new System.Drawing.Point(217, 99);
-            this.btn_cat1.Name = "btn_cat1";
-            this.btn_cat1.Size = new System.Drawing.Size(247, 40);
-            this.btn_cat1.TabIndex = 0;
-            this.btn_cat1.Text = "Categoria 1";
-            this.btn_cat1.TextColor = System.Drawing.Color.White;
-            this.btn_cat1.UseVisualStyleBackColor = false;
-            // 
-            // btn_cat2
-            // 
-            this.btn_cat2.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat2.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btn_cat2.BorderRadius = 40;
-            this.btn_cat2.BorderSize = 0;
-            this.btn_cat2.FlatAppearance.BorderSize = 0;
-            this.btn_cat2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cat2.ForeColor = System.Drawing.Color.White;
-            this.btn_cat2.Location = new System.Drawing.Point(217, 145);
-            this.btn_cat2.Name = "btn_cat2";
-            this.btn_cat2.Size = new System.Drawing.Size(247, 40);
-            this.btn_cat2.TabIndex = 1;
-            this.btn_cat2.Text = "Categoria 2";
-            this.btn_cat2.TextColor = System.Drawing.Color.White;
-            this.btn_cat2.UseVisualStyleBackColor = false;
-            // 
-            // btn_cat3
-            // 
-            this.btn_cat3.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat3.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat3.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btn_cat3.BorderRadius = 40;
-            this.btn_cat3.BorderSize = 0;
-            this.btn_cat3.FlatAppearance.BorderSize = 0;
-            this.btn_cat3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cat3.ForeColor = System.Drawing.Color.White;
-            this.btn_cat3.Location = new System.Drawing.Point(217, 191);
-            this.btn_cat3.Name = "btn_cat3";
-            this.btn_cat3.Size = new System.Drawing.Size(247, 40);
-            this.btn_cat3.TabIndex = 2;
-            this.btn_cat3.Text = "Categoria 3";
-            this.btn_cat3.TextColor = System.Drawing.Color.White;
-            this.btn_cat3.UseVisualStyleBackColor = false;
-            // 
-            // btn_cat4
-            // 
-            this.btn_cat4.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat4.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
-            this.btn_cat4.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btn_cat4.BorderRadius = 40;
-            this.btn_cat4.BorderSize = 0;
-            this.btn_cat4.FlatAppearance.BorderSize = 0;
-            this.btn_cat4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_cat4.ForeColor = System.Drawing.Color.White;
-            this.btn_cat4.Location = new System.Drawing.Point(217, 237);
-            this.btn_cat4.Name = "btn_cat4";
-            this.btn_cat4.Size = new System.Drawing.Size(247, 40);
-            this.btn_cat4.TabIndex = 3;
-            this.btn_cat4.Text = "Categoria 4";
-            this.btn_cat4.TextColor = System.Drawing.Color.White;
-            this.btn_cat4.UseVisualStyleBackColor = false;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(269, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 55);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Menú";
             // 
             // btn_cat5
             // 
@@ -143,16 +82,77 @@
             this.btn_cat5.TextColor = System.Drawing.Color.White;
             this.btn_cat5.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // btn_cat4
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(269, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 55);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Menú";
+            this.btn_cat4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat4.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat4.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_cat4.BorderRadius = 40;
+            this.btn_cat4.BorderSize = 0;
+            this.btn_cat4.FlatAppearance.BorderSize = 0;
+            this.btn_cat4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cat4.ForeColor = System.Drawing.Color.White;
+            this.btn_cat4.Location = new System.Drawing.Point(217, 237);
+            this.btn_cat4.Name = "btn_cat4";
+            this.btn_cat4.Size = new System.Drawing.Size(247, 40);
+            this.btn_cat4.TabIndex = 3;
+            this.btn_cat4.Text = "Categoria 4";
+            this.btn_cat4.TextColor = System.Drawing.Color.White;
+            this.btn_cat4.UseVisualStyleBackColor = false;
+            // 
+            // btn_cat3
+            // 
+            this.btn_cat3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat3.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_cat3.BorderRadius = 40;
+            this.btn_cat3.BorderSize = 0;
+            this.btn_cat3.FlatAppearance.BorderSize = 0;
+            this.btn_cat3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cat3.ForeColor = System.Drawing.Color.White;
+            this.btn_cat3.Location = new System.Drawing.Point(217, 191);
+            this.btn_cat3.Name = "btn_cat3";
+            this.btn_cat3.Size = new System.Drawing.Size(247, 40);
+            this.btn_cat3.TabIndex = 2;
+            this.btn_cat3.Text = "Categoria 3";
+            this.btn_cat3.TextColor = System.Drawing.Color.White;
+            this.btn_cat3.UseVisualStyleBackColor = false;
+            // 
+            // btn_cat2
+            // 
+            this.btn_cat2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_cat2.BorderRadius = 40;
+            this.btn_cat2.BorderSize = 0;
+            this.btn_cat2.FlatAppearance.BorderSize = 0;
+            this.btn_cat2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cat2.ForeColor = System.Drawing.Color.White;
+            this.btn_cat2.Location = new System.Drawing.Point(217, 145);
+            this.btn_cat2.Name = "btn_cat2";
+            this.btn_cat2.Size = new System.Drawing.Size(247, 40);
+            this.btn_cat2.TabIndex = 1;
+            this.btn_cat2.Text = "Categoria 2";
+            this.btn_cat2.TextColor = System.Drawing.Color.White;
+            this.btn_cat2.UseVisualStyleBackColor = false;
+            // 
+            // btn_cat1
+            // 
+            this.btn_cat1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btn_cat1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_cat1.BorderRadius = 40;
+            this.btn_cat1.BorderSize = 0;
+            this.btn_cat1.FlatAppearance.BorderSize = 0;
+            this.btn_cat1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cat1.ForeColor = System.Drawing.Color.White;
+            this.btn_cat1.Location = new System.Drawing.Point(217, 99);
+            this.btn_cat1.Name = "btn_cat1";
+            this.btn_cat1.Size = new System.Drawing.Size(247, 40);
+            this.btn_cat1.TabIndex = 0;
+            this.btn_cat1.Text = "Categoria 1";
+            this.btn_cat1.TextColor = System.Drawing.Color.White;
+            this.btn_cat1.UseVisualStyleBackColor = false;
             // 
             // Menu
             // 
--- QuizGame/MySQL_base_datos/quiz_game_db.sql
+++ QuizGame/MySQL_base_datos/quiz_game_db.sql
@@ -1,6 +1,3 @@
-DROP DATABASE IF EXISTS quiz_game;
-CREATE DATABASE  IF NOT EXISTS `quiz_game` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `quiz_game`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: quiz_game
@@ -113,7 +110,7 @@ CREATE TABLE `pregunta` (
   PRIMARY KEY (`id_pregunta`),
   KEY `fk_pregunta_categoria_idx` (`id_categoria`),
   CONSTRAINT `fk_pregunta_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,6 +119,7 @@ CREATE TABLE `pregunta` (
 
 LOCK TABLES `pregunta` WRITE;
 /*!40000 ALTER TABLE `pregunta` DISABLE KEYS */;
+INSERT INTO `pregunta` VALUES (1,1,'¿En qué año comenzó la Segunda Guerra Mundial?','texto'),(2,1,'¿Quién fue el primer presidente de México?','texto'),(3,1,'¿Qué cultura mesoamericana es famosa por sus calendarios avanzados?','texto'),(4,1,'¿Quién fue el primer hombre en pisar la luna en 1969?','texto'),(5,1,'¿En qué año descubrió América Cristóbal Colón?','texto'),(6,1,'¿Qué imagen muestra a Nelson Mandela?','imagen'),(7,1,'¿Qué imagen corresponde al sacerdote que dio el Grito de Independencia en 1810?','imagen'),(8,1,'¿Quién era el presidente de México en 2010?','imagen'),(9,1,'¿Quién fue el líder de Alemania durante la Segunda Guerra Mundial?','imagen'),(10,1,'¿Cuál de estas pirámides es la Pirámide del Sol?','imagen'),(11,2,NULL,'texto'),(12,2,NULL,'texto'),(13,2,NULL,'texto'),(14,2,NULL,'texto'),(15,2,NULL,'texto'),(16,2,NULL,'imagen'),(17,2,NULL,'imagen'),(18,2,NULL,'imagen'),(19,2,NULL,'imagen'),(20,2,NULL,'imagen'),(21,3,'¿Qué cantante es conocido como “El rey del pop”?','texto'),(22,3,'¿Qué banda británica interpretó la canción Bohemian Rhapsody?','texto'),(23,3,'¿Quién compuso La Quinta Sinfonía?','texto'),(24,3,'¿Qué cantante dominicano es conocido como El Rey de la Bachata?','texto'),(25,3,'¿Qué artista cantó Despacito?','texto'),(26,3,'¿Quién fue el artista principal del Super Bowl 2021?','imagen'),(27,3,'¿Cuál de estos instrumentos pertenece a la música clásica?','imagen'),(28,3,'¿Qué artista se hizo famoso con la canción La Bicicleta?','imagen'),(29,3,'¿Cuál portada corresponde al álbum El Último Tour Del Mundo?','imagen'),(30,3,'¿Quién cantó Waka Waka en el Mundial 2010?','imagen'),(31,4,'¿Cuál es el planeta más grande del sistema solar?','texto'),(32,4,'¿Cuál es el planeta más cercano al Sol?','texto'),(33,4,'¿Cuál es el planeta más lejano del Sol?','texto'),(34,4,'¿Cuál es el sexto planeta desde el Sol?','texto'),(35,4,'¿Cuál es el satélite natural de la Tierra?','texto'),(36,4,'¿Qué imagen corresponde al planeta más grande del sistema solar?','imagen'),(37,4,'¿Cuál de estas imágenes corresponde a la superficie de Mercurio?','imagen'),(38,4,'¿Qué imagen corresponde a nuestra galaxia?','imagen'),(39,4,'¿Qué imagen corresponde al tercer planeta desde el Sol?','imagen'),(40,4,'¿Cuál fase de la Luna se llama cuarto menguante?','imagen'),(41,5,'¿Qué significa RAM?','texto'),(42,5,'¿Quién fabrica las tarjetas gráficas Radeon?','texto'),(43,5,'¿Cuál fue la primera red social importante?','texto'),(44,5,'¿Qué lenguaje se usa para apps iOS?','texto'),(45,5,'¿Quién es el padre de la computación?','texto'),(46,5,'¿Cuál logo corresponde al segundo logo de Apple?','imagen'),(47,5,'¿Cuál empresa NO fabrica CPU?','imagen'),(48,5,'¿Quién es el CEO actual de Apple?','imagen'),(49,5,'¿Qué plataforma popularizó videos de 6 segundos?','imagen'),(50,5,'¿Quién fundó Nvidia?','imagen');
 /*!40000 ALTER TABLE `pregunta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,7 +139,7 @@ CREATE TABLE `respuesta` (
   PRIMARY KEY (`id_respuesta`),
   KEY `id_pregunta` (`id_pregunta`),
   CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`id_pregunta`) REFERENCES `pregunta` (`id_pregunta`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,6 +148,7 @@ CREATE TABLE `respuesta` (
 
 LOCK TABLES `respuesta` WRITE;
 /*!40000 ALTER TABLE `respuesta` DISABLE KEYS */;
+INSERT INTO `respuesta` VALUES (1,1,'1945',NULL,0),(2,1,'1935',NULL,0),(3,1,'1939',NULL,1),(4,1,'1941',NULL,0),(5,2,'Benito Juárez',NULL,0),(6,2,'Porfirio Díaz',NULL,0),(7,2,'Vicente Guerrero',NULL,0),(8,2,'Guadalupe Victoria',NULL,1),(9,3,'Azteca',NULL,0),(10,3,'Maya',NULL,1),(11,3,'Tolteca',NULL,0),(12,3,'Zapoteca',NULL,0),(13,4,'Yuri Gagarin',NULL,0),(14,4,'Buzz Aldrin',NULL,0),(15,4,'Neil Armstrong',NULL,1),(16,4,'Alan Shepard',NULL,0),(17,5,'1482',NULL,0),(18,5,'1492',NULL,1),(19,5,'1502',NULL,0),(20,5,'1495',NULL,0),(21,6,'',NULL,0),(22,6,'',NULL,0),(23,6,'',NULL,0),(24,6,'',NULL,0),(25,7,'',NULL,0),(26,7,'',NULL,0),(27,7,'',NULL,0),(28,7,'',NULL,0),(29,8,'',NULL,0),(30,8,'',NULL,0),(31,8,'',NULL,0),(32,8,'',NULL,0),(33,9,'',NULL,0),(34,9,'',NULL,0),(35,9,'',NULL,0),(36,9,'',NULL,0),(37,10,'',NULL,0),(38,10,'',NULL,0),(39,10,'',NULL,0),(40,10,'',NULL,0),(41,11,'',NULL,0),(42,11,'',NULL,0),(43,11,'',NULL,0),(44,11,'',NULL,0),(45,12,'',NULL,0),(46,12,'',NULL,0),(47,12,'',NULL,0),(48,12,'',NULL,0),(49,13,'',NULL,0),(50,13,'',NULL,0),(51,13,'',NULL,0),(52,13,'',NULL,0),(53,14,'',NULL,0),(54,14,'',NULL,0),(55,14,'',NULL,0),(56,14,'',NULL,0),(57,15,'',NULL,0),(58,15,'',NULL,0),(59,15,'',NULL,0),(60,15,'',NULL,0),(61,16,'',NULL,0),(62,16,'',NULL,0),(63,16,'',NULL,0),(64,16,'',NULL,0),(65,17,'',NULL,0),(66,17,'',NULL,0),(67,17,'',NULL,0),(68,17,'',NULL,0),(69,18,'',NULL,0),(70,18,'',NULL,0),(71,18,'',NULL,0),(72,18,'',NULL,0),(73,19,'',NULL,0),(74,19,'',NULL,0),(75,19,'',NULL,0),(76,19,'',NULL,0),(77,20,'',NULL,0),(78,20,'',NULL,0),(79,20,'',NULL,0),(80,20,'',NULL,0),(81,21,'Elvis Presley',NULL,0),(82,21,'Prince Royce',NULL,0),(83,21,'Michael Jackson',NULL,1),(84,21,'Justin Timberlake',NULL,0),(85,22,'The Beatles',NULL,0),(86,22,'Queen',NULL,1),(87,22,'The Who',NULL,0),(88,22,'Oasis',NULL,0),(89,23,'Wolfgang Amadeus Mozart',NULL,0),(90,23,'Johann Sebastian Bach',NULL,0),(91,23,'Ludwig van Beethoven',NULL,1),(92,23,'Franz Schubert',NULL,0),(93,24,'Eminem',NULL,1),(94,24,'50 Cent',NULL,0),(95,24,'Dr. Dre',NULL,0),(96,24,'Snoop Dogg',NULL,0),(97,25,'Prince Royce',NULL,0),(98,25,'Romeo Santos',NULL,1),(99,25,'Juan Luis Guerra',NULL,0),(100,25,'Frank Reyes',NULL,0),(101,26,'',NULL,0),(102,26,'',NULL,0),(103,26,'',NULL,0),(104,26,'',NULL,0),(105,27,'',NULL,0),(106,27,'',NULL,0),(107,27,'',NULL,0),(108,27,'',NULL,0),(109,28,'',NULL,0),(110,28,'',NULL,0),(111,28,'',NULL,0),(112,28,'',NULL,0),(113,29,'',NULL,0),(114,29,'',NULL,0),(115,29,'',NULL,0),(116,29,'',NULL,0),(117,30,'',NULL,0),(118,30,'',NULL,0),(119,30,'',NULL,0),(120,30,'',NULL,0),(121,31,'Saturno',NULL,0),(122,31,'Urano',NULL,0),(123,31,'Neptuno',NULL,0),(124,31,'Júpiter',NULL,1),(125,32,'Venus',NULL,0),(126,32,'Tierra',NULL,0),(127,32,'Mercurio',NULL,1),(128,32,'Marte',NULL,0),(129,33,'Neptuno',NULL,1),(130,33,'Saturno',NULL,0),(131,33,'Urano',NULL,0),(132,33,'Júpiter',NULL,0),(133,34,'Júpiter',NULL,0),(134,34,'Saturno',NULL,1),(135,34,'Urano',NULL,0),(136,34,'Neptuno',NULL,0),(137,35,'Andrómeda',NULL,0),(138,35,'La Luna',NULL,1),(139,35,'Sirio',NULL,0),(140,35,'Vía Láctea',NULL,0),(141,36,'',NULL,0),(142,36,'',NULL,0),(143,36,'',NULL,0),(144,36,'',NULL,0),(145,37,'',NULL,0),(146,37,'',NULL,0),(147,37,'',NULL,0),(148,37,'',NULL,0),(149,38,'',NULL,0),(150,38,'',NULL,0),(151,38,'',NULL,0),(152,38,'',NULL,0),(153,39,'',NULL,0),(154,39,'',NULL,0),(155,39,'',NULL,0),(156,39,'',NULL,0),(157,40,'',NULL,0),(158,40,'',NULL,0),(159,40,'',NULL,0),(160,40,'',NULL,0),(161,41,'Random Access Memory',NULL,1),(162,41,'Rapid Access Machine',NULL,0),(163,41,'Read Access Memory',NULL,0),(164,41,'Real Application Memory',NULL,0),(165,42,'Intel',NULL,0),(166,42,'Nvidia',NULL,0),(167,42,'AMD',NULL,1),(168,42,'Qualcomm',NULL,0),(169,43,'Facebook',NULL,0),(170,43,'MySpace',NULL,1),(171,43,'Twitter',NULL,0),(172,43,'LinkedIn',NULL,0),(173,44,'Python',NULL,0),(174,44,'Java',NULL,0),(175,44,'Swift',NULL,1),(176,44,'Ruby',NULL,0),(177,45,'Alan Turing',NULL,1),(178,45,'Bill Gates',NULL,0),(179,45,'Steve Jobs',NULL,0),(180,45,'Charles Babbage',NULL,0),(181,46,'',NULL,0),(182,46,'',NULL,0),(183,46,'',NULL,0),(184,46,'',NULL,0),(185,47,'',NULL,0),(186,47,'',NULL,0),(187,47,'',NULL,0),(188,47,'',NULL,0),(189,48,'',NULL,0),(190,48,'',NULL,0),(191,48,'',NULL,0),(192,48,'',NULL,0),(193,49,'',NULL,0),(194,49,'',NULL,0),(195,49,'',NULL,0),(196,49,'',NULL,0),(197,50,'',NULL,0),(198,50,'',NULL,0),(199,50,'',NULL,0),(200,50,'',NULL,0);
 /*!40000 ALTER TABLE `respuesta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -162,4 +161,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-12 17:59:59
+-- Dump completed on 2026-03-14 17:35:26
